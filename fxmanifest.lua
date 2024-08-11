895042b147ae04c99881c1fc14d17fa4ce4236c2
StevoScripts <119699613+stevoscriptsteam@users.noreply.github.com>
2024-08-11 12:24:51 +1000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 
 author 'StevoScripts | steve'
 description 'Advanced Portable Crafting System with props, blueprints and more!'
-version '2.0.0'
+version '2.0.1'
 
 shared_scripts {
   'config.lua',
